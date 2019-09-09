5641d96ec6590e0e56ff72966e21ce84534b9e21
Jüri Jõul <jyri.joul@gmail.com>
2019-09-09 18:41:46 +0300

    Roboti liikumine (translatsioon) enam-vähem valmis. Saab klaviatuurilt juhtida.

--- mainboard_comm.py
+++ mainboard_comm.py
@@ -1,5 +1,7 @@
 import serial
 from math import *
+import keyboard
+import time
 
 # Initialize serial
 ser = serial.Serial("COM4", 115200)
@@ -8,6 +10,7 @@ ser = serial.Serial("COM4", 115200)
 # wheelSpeedToMainboardUnits = gearboxReductionRatio * encoderEdgesPerMotorRevolution /
 # (2 * PI * wheelRadius * pidControlFrequency)
 wheel_speed_to_mainboard_units = 18.75 * 64 / (2 * pi * 0.035 * 60)
+#print("Wheel speed to mainboard units:", wheel_speed_to_mainboard_units)
 
 
 def robot_speed(robot_speed_x, robot_speed_y):
@@ -19,26 +22,33 @@ def robot_direction_angle(robot_speed_x, robot_speed_y):
 
 
 def wheel_linear_velocity(robot_speed, robot_direction_angle, wheel_angle):
-    return robot_speed * cos(robot_direction_angle - wheel_angle)
+    #print("Robot_speed:", robot_speed, "; angle:", robot_direction_angle , "; cos:", cos(robot_direction_angle - wheel_angle))
+    return robot_speed * cos(robot_direction_angle - radians(wheel_angle))
 
 
-def wheel_angular_speed_maiboard_units(wheel_linear_velocity, wheel_speed_to_mainboard_units):
+def wheel_angular_speed_mainboard_units(wheel_linear_velocity, wheel_speed_to_mainboard_units):
+    #print("Wheel linear velocity:", wheel_linear_velocity)
     return wheel_linear_velocity * wheel_speed_to_mainboard_units
 
 
-def get_motor_speeds(robot_speed, direction_angle):
+def get_motor_speeds(robot_speed_x, robot_speed_y):
+    rbt_spd = robot_speed(robot_speed_x, robot_speed_y)
+    dir_ang = robot_direction_angle(robot_speed_x, robot_speed_y)
+    print("Speed:", rbt_spd, "; angle:", degrees(dir_ang))
+
     motors = [0, 0, 0]
-    motors[0] = wheel_angular_speed_maiboard_units(wheel_linear_velocity(robot_speed, direction_angle, 120),
-                                                   wheel_speed_to_mainboard_units)
-    motors[1] = wheel_angular_speed_maiboard_units(wheel_linear_velocity(robot_speed, direction_angle, 0),
-                                                   wheel_speed_to_mainboard_units)
-    motors[2] = wheel_angular_speed_maiboard_units(wheel_linear_velocity(robot_speed, direction_angle, 240),
-                                                   wheel_speed_to_mainboard_units)
+    motors[0] = wheel_angular_speed_mainboard_units(wheel_linear_velocity(rbt_spd, dir_ang, 120),
+                                                    wheel_speed_to_mainboard_units)
+    motors[1] = wheel_angular_speed_mainboard_units(wheel_linear_velocity(rbt_spd, dir_ang, 0),
+                                                    wheel_speed_to_mainboard_units)
+    motors[2] = wheel_angular_speed_mainboard_units(wheel_linear_velocity(rbt_spd, dir_ang, 240),
+                                                    wheel_speed_to_mainboard_units)
     return motors
 
 
 def send_to_mainboard(motors):
-    message = ("sd:" + str(round(motors[0])) + ":" + str(round(motors[1])) + ":" + str(round(motors[2])) + ":0\n").encode("'utf-8")
+    message = ("sd:" + str(round(motors[0])) + ":" + str(round(motors[1])) + ":" + str(
+        round(motors[2])) + ":0\n").encode("'utf-8")
     print(message)
     ser.write(message)
 
@@ -54,8 +64,48 @@ def send_to_mainboard(motors):
     return line
 
 
+def send_to_mainboard_debug(motors):
+    message = ("sd:" + str(round(motors[0])) + ":" + str(round(motors[1])) + ":" + str(
+        round(motors[2])) + ":0\n").encode("'utf-8")
+    print(message)
+
+
 user_input = ""
 while user_input == "":
+    sleep = 0.5
+    x_speed = 0
+    y_speed = 0
+    speed = 0.2
+    
+    presses = 0
+    presses_max = 2
+    
+    while True:
+        if presses >= presses_max:
+            break
+        if keyboard.is_pressed('a') and presses < presses_max:
+            print("A")
+            x_speed += speed
+            presses += 1
+        if keyboard.is_pressed('s') and presses < presses_max:
+            print("S")
+            y_speed += speed
+            presses += 1
+        if keyboard.is_pressed('w') and presses < presses_max:
+            print("W")
+            y_speed -= speed
+            presses += 1
+        if keyboard.is_pressed('d') and presses < presses_max:
+            print("D")
+            x_speed -= speed
+            presses += 1
+
+    motors = get_motor_speeds(x_speed, y_speed)
+    send_to_mainboard(motors)
+
+    time.sleep(sleep)
+    #user_input = input("Press enter..")
+    
     # Ask for information
     # ser.write("gs\n".encode('utf-8'))
     # ser.write(b"sd:0:10:0:0\n")
@@ -66,8 +116,3 @@ while user_input == "":
     #     line += char
     #     char = ser.read().decode()
     # print(line)
-
-    motors = get_motor_speeds(0.2, 90)
-    send_to_mainboard(motors)
-
-    user_input = input("Press enter..")
